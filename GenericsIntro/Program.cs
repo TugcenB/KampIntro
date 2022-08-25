5b7ba80ed36791e7b7259d64f0bdcc840aa4f43b
BilginTugcenur <92152213+BilginTugcenur@users.noreply.github.com>
2022-08-25 17:55:30 +0300

    -

--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -9,6 +9,13 @@ namespace GenericsIntro
         { 
             MyList<string> isimler = new MyList<string>();
             isimler.Add("Tuğçe");
+            isimler.Add("Talha");
+
+            Console.WriteLine(isimler.Length);
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
         }
     }
 }
