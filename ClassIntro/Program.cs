da4f5bd25c2398aef0c99e4d9ff1a8bdd6edb91e
BilginTugcenur <92152213+BilginTugcenur@users.noreply.github.com>
2022-08-25 13:16:19 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kurs kurs = new Kurs();
+            kurs.KursAdi = "C#";
+            kurs.Egitmen = "Engin Demiroğ";
+            kurs.izlenmeOrani = 68;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "Java";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.izlenmeOrani = 64;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.Egitmen = "Engin Demiroğ";
+            kurs3.izlenmeOrani = 80;
+
+            Console.WriteLine(kurs.KursAdi+" "+kurs.Egitmen);
+            Console.WriteLine(kurs1.KursAdi + " " + kurs1.Egitmen);
+            Console.WriteLine(kurs3.KursAdi + " " + kurs3.Egitmen);
+            
+            Kurs[] kurslar = new Kurs[] { kurs, kurs1,kurs3 };
+            foreach (var a in kurslar)
+            {
+                Console.WriteLine(a.KursAdi+" : "+a.Egitmen);
+            }
+        }
+    }
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int izlenmeOrani { get; set; }
+    }
+}
---
+++ DegerveReferansTipler/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DegerveReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 30;
+            a = b;
+            b = 65;
+            Console.WriteLine(a);//30
+
+            int[] aa=new int[] {10,20,30}; 
+            int[] bb=new int[] {100,200,300};
+            aa = bb;
+            bb[0] = 999;
+            Console.WriteLine(aa[0]);//999
+
+            //int, decimal, double, float, bool=değer tip
+            //array,class, interface=referans tip
+
+            //stack - a=10x->30 b=30x->65
+            //heap - 
+
+            //stack -    aa/101x->102                                        bb/102
+            //heap -  [10,20,30]/101xmove to garbage collector         [100->999,200,300]/102
+        }
+    }
+}
