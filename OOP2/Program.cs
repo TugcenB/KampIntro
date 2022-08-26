10524b1d58d7ba133ea7bdb5169d2ff26a4d3003
BilginTugcenur <92152213+BilginTugcenur@users.noreply.github.com>
2022-08-26 15:08:56 +0300

    26.08.2022

---
+++ OOP2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri = new GercekMusteri();
+            musteri.Id = 1;
+            musteri.MusteriNo = "12345";
+            musteri.Adi = "Tuğçe";
+            musteri.Soyadi = "Bilgin";
+            musteri.TcNo = "12345678910";
+
+
+            //Kodlama.io
+            TuzelMusteri musteri1= new TuzelMusteri();
+            musteri1.Id = 2;
+            musteri1.MusteriNo = "54321";
+            musteri1.SirketAdi = "Kodlama.io";
+            musteri1.VergiNo = "1234567890";
+
+            Customer musteri2 = new GercekMusteri();
+            Customer musteri3 = new TuzelMusteri();
+            //Hem gerçek hem tüzel   //new ref. no oluşturuyor.
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri);
+            customerManager.Add(musteri1);
+
+        }
+    }
+}
