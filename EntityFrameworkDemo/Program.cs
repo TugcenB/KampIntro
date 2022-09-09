ec144fade2a56351ccb818c85168c128410ff5b2
TugcenB <ttbilgin1@gmail.com>
2022-09-09 17:41:08 +0300

    Added WebAPI

---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+{
+    private static void Main(string[] args)
+        {
+            //ADO.NET
+            //Entity Framework - Bir ORM Object Relational Mapping
+            //NHibernate
+            //Dapper
+            //GetAll();
+            GetByCategoryId(2);
+        }
+
+        private static void GetAll()
+        {
+            NorthwindContext context = new NorthwindContext();
+            foreach (var item in context.Products)
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+        private static void GetByCategoryId(int id)
+        {
+            NorthwindContext context = new NorthwindContext();
+
+            var result = context.Products.Where(p=>p.CategoryId==id);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.ProductName);
+            }
+
+        }
+    }
+}
---
+++ tracking.client/IssueDto.cs
@@ -0,0 +1,23 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+public class IssueDto
+{
+    public int Id { get; set; }
+    
+    public string Title { get; set; }
+    
+    public string Description { get; set; }
+    public Priority priority { get; set; }
+    public IssueType issueType { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime? Completed { get; set; }
+    public enum Priority
+    {
+        Low, Medium, High
+    }
+    public enum IssueType
+    {
+        Feature, Bug, Documentation
+    }
+}
