d519fb62da7c822961ff5876cc4a1dfaf93c62b8
TugcenB <ttbilgin1@gmail.com>
2022-09-10 18:53:19 +0300

    Attributes+Reflection

---
+++ Attributes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Customer customer = new Customer { Id=1,Name="jdkjd"};
+        CustomerDal customerDal= new CustomerDal();
+        customerDal.Add(customer);
+    }
+     
+    
+
+}
+[ToTable("TblCustomers")]
+[ToTable("Customers")]
+class Customer
+{
+    public int Id { get; set; }
+    [RequiredProperty]
+    public string Name { get; set; }
+    
+}
+[AttributeUsage(AttributeTargets.Property,AllowMultiple =true)]
+public class RequiredPropertyAttribute : Attribute
+{
+}
+
+class CustomerDal
+{
+    [Obsolete("Don't use Add")]
+    public void Add(Customer customer)
+    {
+        Console.WriteLine("{0} , {1} is added", customer.Id, customer.Name);
+
+    }
+}
+[AttributeUsage(AttributeTargets.Class,AllowMultiple =true)]
+class ToTableAttribute : Attribute
+{
+    private string _tableName;
+
+    public ToTableAttribute(string tableName)
+    {
+        _tableName = tableName;
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Reflection;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        //DortIslem dortIslem = new DortIslem(2,3);
+        //Console.WriteLine(dortIslem.Add2());
+        var type = typeof(DortIslem);
+        var dortIslem=(DortIslem)Activator.CreateInstance(type,6,7);
+        Console.WriteLine(dortIslem.Add(1,2));
+
+        var instance = Activator.CreateInstance(type, 6, 7);
+        MethodInfo methodInfo = instance.GetType().GetMethod("Add2");
+        var result=methodInfo.Invoke(instance,null);
+        Console.WriteLine(result);
+
+        var metodlar = type.GetMethods();
+        foreach (var info in metodlar)
+        {
+            Console.WriteLine("Metod Adı: "+info.Name);
+            foreach (var parameterInfo in info.GetParameters())
+            {
+                Console.WriteLine(parameterInfo.Name);
+            }
+            foreach (var attribute in info.GetCustomAttributes())
+            {
+                Console.WriteLine("Attribute Name: "+attribute.GetType().Name);
+            }
+        }
+        Console.ReadLine();
+        
+    }
+}
+public class DortIslem
+{
+    private int _sayi1;
+    private int _sayi2;
+
+    public DortIslem(int sayi1, int sayi2)
+    {
+        _sayi1 = sayi1;
+        _sayi2 = sayi2;
+    }
+
+    public int Add(int sayi1, int sayi2)
+    {
+        return sayi1+sayi2;
+    }
+    public int Carp(int sayi1, int sayi2)
+    {
+        return sayi1*sayi2;
+    }
+    public int Add2()
+    {
+        return _sayi1+_sayi2;
+    }
+    [MethodName("Carpma")]
+    public int Carp2()
+    {
+        return _sayi1*_sayi2;
+    }
+}
+public class MethodNameAttribute : Attribute
+{
+    public MethodNameAttribute(string name)
+    {
+
+    }
+}
